47ab2b6cf68965f57b49166d3fad13d5edc59130
huyue <huyue@didihu.com.cn>
2019-01-12 21:15:35 +0800

    form组件初步完成

--- components/form/form.js
+++ components/form/form.js
@@ -3,18 +3,18 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    orderList: Array
+    list: Array,
+    submit: Boolean,
+    reset: Boolean
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    date: '请选择日期',
-    time: '请选择时间',
-    region: ['请选择'],
-    formList: [],
-    
+    date: '',
+    time: '',
+    region: []
   },
 
   /**
@@ -26,31 +26,36 @@ Component({
       console.log(e)
       console.log('form发生了submit事件，携带数据为：', e.detail.value)
     },
+    // 重置
     formReset() {
       console.log('form发生了reset事件')
     },
-    // picker确定
+    // 省市picker确定
     bindRegionChange: function (e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
       this.setData({
         region: e.detail.value
       })
     },
+    // 单列picker确定
     bindPickerChange: function (e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
       this.setData({
-        i: e.detail.value
+        j: e.detail.value
       })
     }, 
+    // 日期picker确定
     bindDateChange: function (e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
       this.setData({
         date: e.detail.value
       })
     },
+    // 单选框picker确定
     radioChange(e) {
       console.log('radio发生change事件，携带value值为：', e.detail.value)
     },
+    // 时间picker确定
     bindTimeChange(e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
       this.setData({
--- waitingOrder/pages/order/order.js
+++ waitingOrder/pages/order/order.js
@@ -2,91 +2,147 @@ const app = getApp()
 
 Page({
   data: {
-    orderList: [
+    date: '',
+    time: '',
+    region: [],
+    submit: true,
+    list: [{
+        title: '订单详情',
+        childList: [{
+          label: '店面名称',
+          name: 'storeName',
+          value: '深圳奥迪专卖店',
+          isdisabled: true,
+          isInput: true
+        }, {
+          label: '联系人',
+          name: 'contact',
+          isdisabled: false,
+          isInput: true,
+          placeholder: '请填写您的称呼'
+        }, {
+          label: '联系电话',
+          name: 'contactNumber',
+          isdisabled: false,
+          isInput: true,
+          placeholder: '请填写您的手机'
+        }, {
+          label: '所在城区',
+          name: 'cityArea',
+          isdisabled: false,
+          isCity: true,
+          placeholder: '请选择城区'
+        }, {
+          label: '详细地址',
+          name: 'address',
+          isdisabled: false,
+          isInput: true,
+          placeholder: '请填写上门安装的详细地址'
+        }, {
+          label: '安装日期',
+          name: 'installationDate',
+          isdisabled: false,
+          isDate: true,
+          startDate: '2015-09-01',
+          endDate: '2017-09-01',
+          placeholder: '请选择日期'
+        }, {
+          label: '安装时间',
+          name: 'installationTime',
+          isdisabled: false,
+          isTime: true,
+          startTime: '09:01',
+          endTime: '21:01',
+          placeholder: '请选择时间'
+        }, {
+          label: '安装类型',
+          name: 'installationType',
+          isdisabled: false,
+          isGeneral: true,
+          placeholder: '请选择类型',
+          array: ['美国', '中国', '巴西', '日本'],
+          objectArray: [
+            {
+              id: 0,
+              name: '美国'
+            },
+            {
+              id: 1,
+              name: '中国'
+            },
+            {
+              id: 2,
+              name: '巴西'
+            },
+            {
+              id: 3,
+              name: '日本'
+            }
+          ]
+        }, {
+          label: '检修原因',
+          name: 'maintenanceReasons',
+          isdisabled: false,
+          isTextarea: true,
+          placeholder: '请输入检修原因'
+        }, {
+          label: '检修类型',
+          name: 'maintenanceType',
+          isdisabled: false,
+          isRadio: true,
+          radioList: [{
+              name: 'USA',
+              value: '美国'
+            },
+            {
+              name: 'CHN',
+              value: '中国',
+              checked: 'true'
+            },
+            {
+              name: 'BRA',
+              value: '巴西'
+            }
+          ]
+        }]
+      },
       {
-        label: '店面名称',
-        name: 'storeName',
-        isdisabled: true,
-        isInput: true
+        title: '车辆信息',
+        childList: [{
+          label: '品牌车型',
+          name: 'brandModel',
+          isdisabled: false,
+          isGeneral: true,
+          array: ['美国', '中国', '巴西', '日本']
+        }, {
+          label: '车辆颜色',
+          name: 'vehicleColor',
+          isdisabled: false,
+          isInput: true,
+          placeholder: '请输入车辆颜色'
+        }, {
+          label: '车架号',
+          name: 'frameNumber',
+          isdisabled: false,
+          isInput: true,
+          placeholder: '请输入17位车架号'
+        }]
       }, {
-        label: '联系人',
-        name: 'contact',
-        isdisabled: false,
-        isInput: true
-      }, {
-        label: '联系电话',
-        name: 'contactNumber',
-        isdisabled: false,
-        isInput: true
-      }, {
-        label: '所在城区',
-        name: 'cityArea',
-        isdisabled: false,
-        isCity: true,
-      }, {
-        label: '详细地址',
-        name: 'address',
-        isdisabled: false,
-        isInput: true
-      }, {
-        label: '安装日期',
-        name: 'installationDate',
-        isdisabled: false,
-        isDate: true,
-        startDate: '2015-09-01',
-        endDate: '2017-09-01'
-      }, {
-        label: '安装时间',
-        name: 'installationTime',
-        isdisabled: false,
-        isTime: true,
-        startTime: '09:01',
-        endTime: '21:01'
-      },  {
-        label: '安装类型',
-        name: 'installationType',
-        isdisabled: false,
-        isGeneral: true,
-        array: ['美国', '中国', '巴西', '日本'],
-        objectArray: [
-          {
-            id: 0,
-            name: '美国'
-          },
-          {
-            id: 1,
-            name: '中国'
-          },
-          {
-            id: 2,
-            name: '巴西'
-          },
-          {
-            id: 3,
-            name: '日本'
-          }
-        ],
-      }, {
-        label: '检修原因',
-        name: 'maintenanceReasons',
-        isdisabled: false,
-        isTextarea: true,
-        placeholder: '请输入检修原因'
-      }, {
-        label: '检修类型',
-        name: 'maintenanceType',
-        isdisabled: false,
-        isRadio: true,
-        radioList: [
-          { name: 'USA', value: '美国' },
-          { name: 'CHN', value: '中国', checked: 'true' },
-          { name: 'BRA', value: '巴西' }
-        ]
+        title: '车主信息',
+        childList: [{
+          label: '车主姓名',
+          name: 'ownerName',
+          isdisabled: false,
+          isInput: true,
+          placeholder: '请填写车主姓名'
+        }, {
+          label: '车主电话',
+          name: 'carOwnerPhone',
+          isdisabled: false,
+          isInput: true,
+          placeholder: '请填写车主联系方式'
+        }]
       }
     ]
-  },
-  onLoad: function () {
-    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
-    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
-  },
+  }
 })
